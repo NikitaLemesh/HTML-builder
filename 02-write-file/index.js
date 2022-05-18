af7e6d62fdb662d5f804fd2294cd9650b39e4331
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2022-05-18 23:09:00 +0300

    feat: add solution for 1 and 2 tasks(Wen 18.05.22 23.08)

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+const { createWriteStream } = require('fs');
+const path = require('path');
+const { stdin, stdout } = require('process');
+
+const filePath = path.join(__dirname, 'output.txt');
+const writable = createWriteStream(filePath);
+
+stdin.on('data', chunk => {
+  if(chunk.toString().trim() === 'exit') {
+    stdout.write('ggwp');
+    process.exit();
+  } else {
+    stdout.write('write some text\n');
+    writable.write(chunk.toString());
+  }
+});
