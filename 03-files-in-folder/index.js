344ff707534d24c8b106365657291461c7be7cec
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2022-05-19 13:50:45 +0300

    feat: add solution for task 3(Thur 19.05.22 13-50)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const  fs = require('fs/promises');
+const { stat } = require('fs');
+const path = require('path');
+
+(async function() {
+  try {
+    const files = await fs.readdir(path.join(__dirname, 'secret-folder'), {encoding: 'utf8', withFileTypes: true});
+    for await (const file of files) {
+      if(file.isFile()) {
+        const fileName = file.name.split('.')[0];
+        const fileExtention = file.name.split('.')[1];
+        await stat(path.join(__dirname, `secret-folder/${file.name}`), (error, stats) => {
+          if(error) {
+            console.log(error);
+          } else {
+            console.log(`${fileName} - ${fileExtention} - ${stats.size}b`);
+          }
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})();
