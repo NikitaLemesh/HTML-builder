26d6257e8fb9a1a2d2986f08bc7206f96ba1fcbe
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2022-05-20 14:44:09 +0300

    feat: add solution for task 4(Friday 20.05.22 14:44)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+(async function() {
+  try {
+    const filesCopy = path.join(__dirname, 'files-copy');
+    const filesForCopy = path.join(__dirname, 'files');
+    await fs.mkdir(filesCopy, { recursive: true });
+    await fs.rm(filesCopy, { recursive: true });
+    await fs.mkdir(filesCopy, { recursive: true });
+    const files = await fs.readdir(filesForCopy, {encoding: 'utf8', withFileTypes: true});
+    for (const file of files) {
+      const copy = path.join(filesCopy, file.name);
+      const exist = path.join(filesForCopy, file.name);
+      await fs.copyFile(exist, copy);
+    }
+  } catch(err) {
+    console.log(err);
+  }
+})();
